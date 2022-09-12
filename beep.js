be3d035dc3649c5f81a325da49d6dbd2db6ae1f2
kyangc <kyangc@gmail.com>
2022-09-12 17:38:39 +0800

    1 feat: fix wav

--- beep.js
+++ beep.js
@@ -1,10 +1,13 @@
 import * as _player from 'play-sound'
 import { loge } from './log.js'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
 
 const player = _player.default()
 
 export const beep = () => {
-    player.play('beep.wav', function (err) {
+    const wavFilePath = path.dirname(fileURLToPath(import.meta.url)) + '/beep.wav'
+    player.play(wavFilePath, function (err) {
         loge(err)
     })
 }
